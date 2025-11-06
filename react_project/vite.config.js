d8ceaeaea65372a0cf2fcd97fd52e2e8859dbea4
Daniel Main <dragma@gmail.com>
2025-11-06 16:15:42 -0700

    test

--- react_project/vite.config.js
+++ react_project/vite.config.js
@@ -3,7 +3,9 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   
     plugins: [plugin()],
+    base: "/react_project/",
     server: {
         port: 64431,
     }
